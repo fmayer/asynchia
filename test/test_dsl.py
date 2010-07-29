53f9bc98e1246d2d42288a562d2d761acc84ae28
Florian Mayer <flormayer@aim.com>
2010-07-29 19:22:47 +0200

    Extend test suite.

--- test/test_dsl.py
+++ test/test_dsl.py
@@ -4,6 +4,19 @@ import asynchia.ee
 
 from asynchia.dsl import b, SBLFLSE
 
+import itertools
+import inspect
+
+def exhaust(itr):
+    result = []
+    for elem in itr:
+        if inspect.isgenerator(elem):
+            result.append(exhaust(iter(elem)))
+        else:
+            result.append(elem)
+    return result
+
+
 def until_done(fun):
     while True:
         d, s = fun()
@@ -18,3 +31,19 @@ def test_example():
     until_done(lambda: a.add_data(m, 120))
     
     eq_(tuple(a.value), (5, 1, 'ABCDE'))
+
+
+def test_nested():
+    i = [2, 'AB', [5, 'ABCDE']]
+    
+    a = b.B + SBLFLSE(0)
+    c = b.B + SBLFLSE(0) + a
+    
+    d = c.produce((2, 'AB', (5, 'ABCDE')))
+    
+    p = c(None)
+    
+    m = asynchia.ee.MockHandler(inbuf=d + 'FG')
+    until_done(lambda: p.add_data(m, 120))
+    
+    eq_(exhaust(iter(p.value)), i)
