47c4730b71196a45bebc3448048e2c45fed5ae63
Florian Mayer <flormayer@aim.com>
2010-07-29 19:24:22 +0200

    Add copyright header to test_dsl. Order imports.

--- test/test_dsl.py
+++ test/test_dsl.py
@@ -1,11 +1,29 @@
-from nose.tools import eq_, assert_raises
+# -*- coding: us-ascii -*-
 
-import asynchia.ee
+# asynchia - asynchronous networking library
+# Copyright (C) 2009 Florian Mayer
 
-from asynchia.dsl import b, SBLFLSE
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import itertools
 import inspect
+
+from nose.tools import eq_, assert_raises
+
+import asynchia.ee
+from asynchia.dsl import b, SBLFLSE
+
 
 def exhaust(itr):
     result = []
